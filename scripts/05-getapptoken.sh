838b45a5a586b472dc95f1eb485d8a77c176a061
Kevin Dyer <kcd83@users.noreply.github.com>
2017-01-21 22:17:36 +1300

    Trying musicstore and docs cu-final apps in docker

--- scripts/05-getapptoken.sh
+++ scripts/05-getapptoken.sh
@@ -8,4 +8,4 @@ echo "accessing vault on ${VAULT_HOST}"
 #:: vault write -wrap-ttl=60s -f auth/approle/role/musicstore/secret-id
 echo "create 'musicstore' app auth token"
 curl -X POST http://${VAULT_HOST}:8200/v1/auth/approle/role/musicstoreapp/secret-id -H "X-Vault-Token: $ROOT_TOKEN" \
-	| jq . | tee /dev/tty | jq .data.secret_id > app_secret_token
+	| jq . | tee /dev/tty | jq .data.secret_id > app/app_secret_token
---
+++ scripts/app/entrypoint.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -e
+
+if ! command -v jq > /dev/null 2>&1
+then
+  echo installing jq
+  apt-get update
+  apt-get install -y jq
+fi
+
+VAULT_HOST=vault
+#source "${BASH_SOURCE%/*}/../06-getmssqlaccess.sh"
+
+SECRET_TOKEN=$(cat ${BASH_SOURCE%/*}/app_secret_token)
+
+echo "accessing vault on ${VAULT_HOST} as musicstoreapp"
+
+echo "login"
+response=$(curl -X POST http://${VAULT_HOST}:8200/v1/auth/approle/login \
+	-d "{\"role_id\":\"musicstoreapp\",\"secret_id\":$SECRET_TOKEN}")
+echo $response | jq .
+auth_token=$(echo $response | jq -r .auth.client_token)
+
+echo "create 'mssql' credentials"
+response=$(curl http://${VAULT_HOST}:8200/v1/mssql/creds/efgodmode -H "X-Vault-Token: $auth_token")
+echo $response | jq .
+user=$(echo $response | jq -r .data.username)
+pass=$(echo $response | jq -r .data.password)
+
+
+export ConnectionStrings__DefaultConnection="Server=mssql;Database=ContosoUniversity;Trusted_Connection=False;MultipleActiveResultSets=true;User ID=${user};Password=${pass}"
+
+echo "connection string created"
+echo $ConnectionStrings__DefaultConnection
+env
+
+# standard restore and run
+dotnet restore
+dotnet run
